e4caeb6744527c268e5d3717fbb78cdfec012c8f
Dawid <dawid.skreczko@gmail.com>
2019-06-25 14:55:35 +0200

    Views: -work in progress

--- url_cutter/url_cut/views.py
+++ url_cutter/url_cut/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
+from .forms import CutUrlForm
+from .models import CutUrl
+from django.views.generic.edit import CreateView
+from django.views.generic.detail import DetailView
+from django.contrib.messages.views import SuccessMessageMixin
 # Create your views here.
+
+class CutUrlCreateView(SuccessMessageMixin, CreateView):
+	model = CutUrl
+	template_name = 'form.html'
+	form_class = CutUrlForm
+	success_message = "%(url_cutted)s is your link"
+
+class CutUrlDetailView(DetailView):
+	model = CutUrl
+
+	def get(self, request, *args, **kwargs):
+		pass
