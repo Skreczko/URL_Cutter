a10dbde5f0f5e6162b8c4e7ae57c0046ae4c44e0
Dawid <dawid.skreczko@gmail.com>
2019-06-25 22:43:45 +0200

    first commit

--- url_cutter/src/settings.py
+++ url_cutter/src/settings.py
@@ -81,8 +81,17 @@ WSGI_APPLICATION = 'src.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
-DATABASES = { 'default': config('DATABASE_URL', default=default_dburl, cast=dburl),}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': '8_URL_CUTTER',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
+
 
 
 
