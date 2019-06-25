108249f8230048979cf358a487c9bb76d08312c9
Dawid <dawid.skreczko@gmail.com>
2019-06-26 00:50:37 +0200

    added external database v3

--- url_cutter/src/urls.py
+++ url_cutter/src/urls.py
@@ -27,4 +27,4 @@ urlpatterns = [
 
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
