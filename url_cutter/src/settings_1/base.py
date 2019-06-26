20a68ee9ef84c405dee970d38ef4cb313293e41c
Dawid <dawid.skreczko@gmail.com>
2019-06-26 08:35:54 +0200

    setting4: -base -local -production

--- url_cutter/src/settings_1/base.py
+++ url_cutter/src/settings_1/base.py
@@ -13,6 +13,11 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 from decouple import config
 
+
+from django.conf import settings
+
+print(settings.DEBUG)
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
