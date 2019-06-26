e0d9d7c25e977fbdfbfcedea55aa313227e7a2ba
Dawid <dawid.skreczko@gmail.com>
2019-06-26 08:47:46 +0200

    setting7: -base -local -production

--- url_cutter/src/settings.py
+++ url_cutter/src/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['url-cutter-92.herokuapp.com', 'localhost:8000', '127.0.0.1']
 
@@ -147,6 +147,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'static'),
 # ]
